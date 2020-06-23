1e03d1409ebc4c587ea1284912c4867ed0693f80
skibitsky <gleb@skibitsky.com>
2020-06-23 14:11:59 +0300

    eslint fixes

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,16 +1,16 @@
-'use strict';
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
+"use strict";
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
 
-describe('generator-unity-package:app', () => {
+describe("generator-unity-package:app", () => {
   beforeAll(() => {
     return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+      .run(path.join(__dirname, "../generators/app"))
+      .withPrompts({ someAnswer: true });
   });
 
-  it('creates files', () => {
-    assert.file(['package.json']);
+  it("creates files", () => {
+    assert.file(["package.json"]);
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const _ = require('lodash');
-const path = require('path');
-const ini = require('ini');
-const YeomanGenerator = require('yeoman-generator');
+const _ = require("lodash");
+const path = require("path");
+const ini = require("ini");
+const YeomanGenerator = require("yeoman-generator");
 
 module.exports = class Generator extends YeomanGenerator {
   constructor() {
@@ -15,56 +15,59 @@ module.exports = class Generator extends YeomanGenerator {
     normalizeOptions(this.options);
 
     const defaults = getDefaults(this.destinationRoot(), this.options);
-    const existing = this.fs.readJSON('package.json');
+    const existing = this.fs.readJSON("package.json");
 
-    if (!this.options.repository && this.fs.exists('.git/config')) {
-      this.options['skip-repo'] = true;
+    if (!this.options.repository && this.fs.exists(".git/config")) {
+      this.options["skip-repo"] = true;
       _.merge(this.options, getRepositoryInformationFromGit(this.fs));
     }
 
     const options = _.reduce(
       this.options,
-      (memo, v, k) => k.indexOf('skip-') === 0 ? memo : _.extend(memo, {[k]: v}),
+      (memo, v, k) =>
+        k.indexOf("skip-") === 0 ? memo : _.extend(memo, { [k]: v }),
       {}
     );
 
-    if (this.options['skip-test']) {
+    if (this.options["skip-test"]) {
       delete defaults.scripts.test;
     }
 
-    _.merge(
-      this.package,
-      defaults,
-      existing,
-      options
-    );
+    _.merge(this.package, defaults, existing, options);
   }
 
   async prompting() {
     const prompts = [
-      ['name', 'package name'],
-      ['version'],
-      ['displayName', 'user-friendly name'],
-      ['description'],
-      ['unity', 'unity version'],
-      ['repo', 'git repository'],
-      ['keywords', 'keywords (space-delimited)', this.package.keywords ? this.package.keywords.join(' ') : ''],
-      ['author'],
-      ['license']
+      ["name", "package name"],
+      ["version"],
+      ["displayName", "user-friendly name"],
+      ["description"],
+      ["unity", "unity version"],
+      ["repo", "git repository"],
+      [
+        "keywords",
+        "keywords (space-delimited)",
+        this.package.keywords ? this.package.keywords.join(" ") : ""
+      ],
+      ["author"],
+      ["license"]
     ]
       .filter(([name]) => !this.options[`skip-${name}`])
       .map(([name, message = name, defaultV = this.package[name]]) => ({
-        type: 'input',
+        type: "input",
         name,
         message: `${message}:`,
         default: defaultV
       }));
 
+    // TODO: refactor loop
+    /* eslint-disable no-constant-condition */
     while (true) {
+      /* eslint-disable no-await-in-loop */
       const res = await this.prompt(prompts);
 
       if (res.keywords && !res.keywords.match(/^\w?$/)) {
-        res.keywords = res.keywords.split(' ');
+        res.keywords = res.keywords.split(" ");
       }
 
       if (res.repo) {
@@ -77,49 +80,53 @@ module.exports = class Generator extends YeomanGenerator {
       // Strip extraneous props
       pkg = _.reduce(
         [
-          'name',
-          'version',
-          'displayName',
-          'description',
-          'unity',
-          'repository',
-          'bugs',
-          'homepage',
-          'keywords',
-          'author',
-          'license',
-          'dependencies'
+          "name",
+          "version",
+          "displayName",
+          "description",
+          "unity",
+          "repository",
+          "bugs",
+          "homepage",
+          "keywords",
+          "author",
+          "license",
+          "dependencies"
         ],
-        (accum, k) => _.extend(accum, {[k]: pkg[k]}),
+        (accum, k) => _.extend(accum, { [k]: pkg[k] }),
         {}
       );
 
-      const {confirmed} = await this.prompt([{
-        type: 'confirm',
-        name: 'confirmed',
-        message: JSON.stringify(pkg, null, 2) + '\n\nIs this OK?',
-        default: true
-      }]);
+      /* eslint-disable no-await-in-loop */
+      const { confirmed } = await this.prompt([
+        {
+          type: "confirm",
+          name: "confirmed",
+          message: JSON.stringify(pkg, null, 2) + "\n\nIs this OK?",
+          default: true
+        }
+      ]);
 
       if (confirmed) {
         this.package = pkg;
+        /* eslint-disable no-await-in-loop */
         this.companyName = await this.prompt([
-            {
-              type: 'input',
-              name: 'companyName',
-              message: 'company name (for .asmdef)',
-              default: toTitleCase(res.author).replace(/\s/g, ''),
-              store: true
-            }
-          ]);
-        break
+          {
+            type: "input",
+            name: "companyName",
+            message: "company name (for .asmdef)",
+            default: toTitleCase(res.author).replace(/\s/g, ""),
+            store: true
+          }
+        ]);
+        break;
       } else {
         // Set defaults to the values passed, so if only one field was messed up
         // the user doesn't have to retype the whole thing
         prompts.forEach(p => {
           // Keywords
           if (Array.isArray(pkg[p.name])) {
-            p.default = pkg[p.name].join(' ');
+            p.default = pkg[p.name].join(" ");
           } else {
             p.default = pkg[p.name];
           }
@@ -129,7 +136,7 @@ module.exports = class Generator extends YeomanGenerator {
   }
 
   writing() {
-    this.fs.writeJSON(this.destinationPath('package.json'), this.package);
+    this.fs.writeJSON(this.destinationPath("package.json"), this.package);
   }
 };
 
@@ -147,47 +154,47 @@ function getDefaults(fd, options) {
   return _.reduce(
     {
       name: basename,
-      version: '1.0.0',
+      version: "1.0.0",
       displayName: toTitleCase(basename),
-      description: '',
-      unity: '',
+      description: "",
+      unity: "",
       keywords: [],
-      license: 'MIT',
+      license: "MIT",
       dependencies: {}
     },
-    (memo, v, k) => (options[`skip-${k}`] ? memo : _.extend(memo, {[k]: v})),
+    (memo, v, k) => (options[`skip-${k}`] ? memo : _.extend(memo, { [k]: v })),
     {}
   );
 }
 
 function getRepositoryInformationFromGit(fs) {
-  const gitConfigIni = fs.read('.git/config');
+  const gitConfigIni = fs.read(".git/config");
   const gitConfig = ini.parse(gitConfigIni);
-  const url = gitConfig['remote "origin"'] ? gitConfig['remote "origin"'].url : '';
+  const url = gitConfig['remote "origin"']
+    ? gitConfig['remote "origin"'].url
+    : "";
   const returnValue = {
     repository: {
-      type: 'git',
+      type: "git",
       url
     }
   };
   try {
-    const repo = url.match(/github\.com[:/](.+)/i)[1].replace(/\.git$/, '');
-    if (url.includes('github.com')) {
+    const repo = url.match(/github\.com[:/](.+)/i)[1].replace(/\.git$/, "");
+    if (url.includes("github.com")) {
       returnValue.bugs = {
         url: `https://github.com/${repo}/issues`
       };
       returnValue.homepage = `https://github.com/${repo}#readme`;
     }
   } finally {
+    /* eslint-disable no-unsafe-finally */
     return returnValue;
   }
 }
 
 function toTitleCase(string) {
-  return string.replace(
-    /\w\S*/g,
-    txt => {
-      return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
-    }
-  );
+  return string.replace(/\w\S*/g, txt => {
+    return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
+  });
 }
